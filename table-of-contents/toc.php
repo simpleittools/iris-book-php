a463d76b45bbf8a708c1d536dab31f6aed7c0f98
Ryan <wm.ryan.mooney@gmail.com>
2025-04-05 12:22:33 -0800

    clean up initial TOC

---
+++ table-of-contents/toc.php
@@ -0,0 +1,116 @@
+<b>Contents</b> <br/>
+<a href="iris-gos.html#4">What is Iris </a><b></b>
+<br/>
+<b>Getting started<br/></b><a href="iris-gos.html#4">Installation </a><b><br/></b>
+<a href="iris-gos.html#5">Quick start</a> 
+<br/>
+<b>Routing<br/></b><a href="iris-gos.html#7">Middleware</a> <b><br/></b>
+<a href="iris-gos.html#18">API Versioning </a>
+<br/>
+<b>Compression<br/></b><a href="iris-gos.html#22">Index</a> <b></b>
+<br/>
+<b>Redirect<br/></b><a href="iris-gos.html#24">Redirect from Context </a><b><br/></b>
+<a href="iris-gos.html#25">Rewrite Middleware</a> <b><br/></b>
+<a href="iris-gos.html#29">Multi Application Instances</a> 
+<br/>
+<b>View/Templates<br/></b><a href="iris-gos.html#33">Documentation </a><b><br/></b>
+<a href="iris-gos.html#38">Benchmarks</a> <b><br/></b>
+<a href="https://github.com/kataras/iris/tree/main/_examples/view">➲ Examples</a> <b></b>
+<br/>
+<b>File Server<br/></b><a href="iris-gos.html#40">Introduction</a> <b><br/></b>
+<a href="iris-gos.html#46">Listing </a><b>.<br/></b>
+<a href="iris-gos.html#55">In-memory Cache</a> <b><br/></b>
+<a href="iris-gos.html#56">HTTP/2 Push + Embedded + Cache and Compression</a> 
+<b><br/></b><a
+    href="iris-gos.html#59">The PrefixDir function </a><b><br/></b>
+<a href="iris-gos.html#59">Serve files from Context </a><b><br/></b>
+<a href="https://github.com/kataras/iris/tree/main/_examples/file-server">➲ Examples</a> <b></b>
+<br/>
+<b>Localization<br/></b><a href="iris-gos.html#62">Documentation </a><b><br/></b>
+<a href="iris-gos.html#67">Sitemap </a><b><br/></b>
+<a href="https://github.com/kataras/iris/tree/main/_examples/i18n">➲ Examples</a> <b></b>
+<br/>
+<b>Security<br/></b><a href="iris-gos.html#70">Basic Authentication </a><b><br/></b>
+<a href="iris-gos.html#77">CORS</a> <b><br/></b>
+<a href="iris-gos.html#80">Sessions &amp; Cookies</a> <b><br/></b>
+<a href="iris-gos.html#85">CSRF </a><b><br/></b>
+<a href="iris-gos.html#92">JSON Web Tokens</a> <b><br/></b>
+<a href="iris-gos.html#107">Access Control</a> <b>.<br/></b>
+<a href="iris-gos.html#107">Anti-bot CAPTCHA</a> <b>.<br/></b>
+<a href="https://github.com/kataras/iris/tree/main/_examples/auth">➲ Examples</a> <b></b>
+<br/>
+<b>Responses</b><br/>
+<hr/>
+<a href="iris-gos.html#113">Text</a> <b><br/></b>
+<a href="iris-gos.html#113">HTML </a><b>.<br/></b>
+<a href="iris-gos.html#114">Markdown</a> <b>.<br/></b>
+<a href="iris-gos.html#114">XML </a><b><br/></b>
+<a href="iris-gos.html#116">YAML</a> <b><br/></b>
+<a href="iris-gos.html#116">Binary</a> <b>.<br/></b>
+<a href="iris-gos.html#117">JSON </a><b><br/></b>
+<a href="iris-gos.html#119">JSONP </a><b>.<br/></b>
+<a href="iris-gos.html#121">Problem </a><b><br/></b>
+<a href="iris-gos.html#124">Protocol Buffers </a><b><br/></b>
+<a href="iris-gos.html#128">MessagePack </a><b><br/></b>
+<a href="iris-gos.html#128">Gzip </a><b><br/></b>
+<a href="iris-gos.html#129">Content Negotiation</a> <b><br/></b>
+<a href="iris-gos.html#134">Stream</a> <b><br/></b>
+<a href="iris-gos.html#139">Server-Sent Events </a><b>.<br/></b>
+<a href="iris-gos.html#140">HTTP/2 Push </a><b><br/></b>
+<a href="iris-gos.html#142">Recorder</a> <b><br/></b>
+<a href="iris-gos.html#145">Outroduction</a> <b><br/></b>
+<a href="https://github.com/kataras/iris/tree/main/_examples/response-writer">➲ Examples</a> <b></b>
+<br/>
+<b>Requests<br/></b><a href="iris-gos.html#145">Headers </a><b><br/></b>
+<a href="iris-gos.html#147">URL Path Parameters</a> <b><br/></b>
+<a href="iris-gos.html#148">Form </a><b><br/></b>
+<a href="https://github.com/kataras/iris/tree/main/_examples/request-body">➲ Examples</a> <b></b>
+<br/>
+<b>Dependency Injection<br/></b><a href="iris-gos.html#150">Documentation </a><b><br/></b>
+<a href="iris-gos.html#158">Register Dependency from Context </a><b><br/></b>
+<a href="iris-gos.html#161">Inputs</a> <b>.<br/></b>
+<a href="iris-gos.html#161">Outputs </a><b><br/></b>
+<a href="https://github.com/kataras/iris/tree/main/_examples/dependency-injection">➲ Examples</a> <b></b>
+<br/>
+<b>MVC<br/></b><a href="iris-gos.html#162">Quick start</a> <b>.<br/></b>
+<a href="iris-gos.html#172">Documentation </a><b><br/></b>
+<a href="iris-gos.html#180">Handle Errors </a><b><br/></b>
+<a href="iris-gos.html#186">Sessions</a> <b><br/></b>
+<a href="iris-gos.html#187">Websockets </a><b><br/></b>
+<a href="iris-gos.html#189">gRPC </a><b><br/></b>
+<a href="https://github.com/kataras/iris/tree/main/_examples/mvc">➲ Examples</a> <b><br/></b>
+<a href="https://github.com/kataras/iris/tree/main/_examples">Examples</a> <b></b>
+<br/>
+<b>Resources<br/></b><a href="iris-gos.html#193">Starter Kits</a> <b>.<br/></b>
+<a href="iris-gos.html#195">Publications</a> <b><br/></b>
+<a href="iris-gos.html#196">Benchmarks</a> <b><br/></b>
+<a href="iris-gos.html#196">Support </a><b></b>
+<br/>
+<b>Contents</b><br/>
+<hr/>
+<a href="iris-gos.html#197">Host</a> <b><br/></b>
+<a href="iris-gos.html#202">Automatic Public Domain with TLS</a> <b><br/></b>
+<a href="iris-gos.html#204">Configuration</a> <b><br/></b>
+<a href="iris-gos.html#214">Routing</a> <b><br/></b>
+<a href="iris-gos.html#220">Path Parameter Types </a><b><br/></b>
+<a href="iris-gos.html#225">Reverse Lookups</a> <b><br/></b>
+<a href="iris-gos.html#227">Handle HTTP errors </a><b><br/></b>
+<a href="iris-gos.html#231">Subdomains</a> <b><br/></b>
+<a href="iris-gos.html#233">Wrap the Router </a><b><br/></b>
+<a href="iris-gos.html#236">Context Methods </a><b><br/></b>
+<a href="iris-gos.html#257">HTTP Method Override </a><b>.<br/></b>
+<a href="iris-gos.html#259">HTTP Referrer</a> <b><br/></b>
+<a href="iris-gos.html#260">URL Query Parameters</a> <b><br/></b>
+<a href="iris-gos.html#262">Forms</a> <b>.<br/></b>
+<a href="iris-gos.html#267">Model Validation </a><b><br/></b>
+<a href="iris-gos.html#270">Cache </a><b>.<br/></b>
+<a href="iris-gos.html#272">Cookies</a> <b><br/></b>
+<a href="iris-gos.html#275">Sessions</a> <b><br/></b>
+<a href="iris-gos.html#277">Database</a> <b><br/></b>
+<a href="iris-gos.html#280">Flash Messages </a><b><br/></b>
+<a href="iris-gos.html#281">Websockets </a><b><br/></b>
+<a href="iris-gos.html#282">Sitemap </a><b><br/></b>
+<a href="iris-gos.html#283">Localization</a> <b><br/></b>
+<a href="iris-gos.html#289">Testing </a><b></b>
+<br/>
+<hr/>
